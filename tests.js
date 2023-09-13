e87d359635665bfbf76fdb05259f0ada248a216d
Ben Heller <benheller1016@gmail.com>
2023-09-13 15:18:25 -0500

    Added the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -1,3 +1,10 @@
+// Unit tests for sayHello function
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+});
+
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
